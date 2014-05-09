5399b568527114714d59c5f51f03dfe3e105ccfb
feblehober123 <sammansfield1@gmail.com>
2014-05-09 07:58:05 -0400

    Changed urls to lupchan.org

--- fourch/fourch.py
+++ fourch/fourch.py
@@ -2,16 +2,16 @@
 from ._version import __version__
 
 urls = {
-    "api": "a.4cdn.org",
-    "boards": "boards.4chan.org",
-    "images": "i.4cdn.org",
-    "thumbs": "t.4cdn.org",
+    "api": "lupchan.org",
+    "boards": "lupchan.org",
+    "images": "lupchan.org",
+    "thumbs": "lupchan.org",
 
     # These are tacked to the end of the api url after formatting.
     "api_board": "/{board}/{page}.json",
-    "api_thread": "/{board}/thread/{thread}.json",
+    "api_thread": "/{board}/res/{thread}.json",
     "api_threads": "/{board}/threads.json",
-    "api_catalog": "/{board}/catalog.json"
+    "api_catalog": "/{board}/catalog.json"  #not sure if this works, I'll just leave it
 }
 
 
