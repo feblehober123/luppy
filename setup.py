70b73113d55eb6bb4b373966d3caa2ae39e36669
feblehober123 <sammansfield1@gmail.com>
2014-05-09 07:52:54 -0400

    Changed stuff to be lupchan specific

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-"""fourch
+"""luppy
 ======
 
 .. _docs: https://4ch.readthedocs.org
 .. _repo: https://github.com/plausibility/4ch
 
-fourch (stylized as 4ch) is a wrapper to the 4chan JSON API, provided by moot. It allows you to interact with 4chan (in a READONLY way) easily through your scripts.
-
-Originally <strike>stolen</strike> forked from `e000/py-4chan <https://github.com/e000/py-4chan>`_, but then I moved repos and renamed stuff since I'm pretty bad about that.
+luppy is a wrapper to the lupchan/tinyboard 4chan-style JSON API, provided by nanasi. It allows you to interact with lupchan (in a READONLY way) easily through your scripts.
+Currently this will not work at all because nanasi still needs to update tinyboard and enable API.
+In the future I plan to include a posting method since lupchan has no captchas.
+luppy is a fork of `plausibility/4ch <https://github.com/plausibility/4ch>`_. Almost all of the code is not mine, so refer to 4ch for usage and things.
 
 Requirements
 ------------
@@ -18,17 +19,18 @@ Notes
 -----
 
 - This isn't guaranteed to work all the time; after all, the API may change, and 4ch will have to be updated accordingly.
-- If a feature is missing, open an issue on the `repo`_, and it may well be implemented.
+- If a feature is missing, open an issue on the `4ch repo`_, and it may well be implemented.
+- If the feature is lupchan/tinyboard specific, open it on the luppy repo instead.
 
 Running / Usage
 ---------------
 
-- Install & import: ``$ pip install 4ch``, ``import fourch``
-- See the `docs`_
+- Currently no installation method
+- See the `4ch docs`_
 
 Contributing
 ------------
-If you're interested in contributing to the usability of 4ch, or just want to give away stars, you can visit the 4ch github `repo`_.
+If you're interested in contributing to the usability of luppy, or just want to give away stars, you can visit the 4ch github `repo`_.
 """
 from setuptools import setup
 
@@ -37,18 +39,18 @@ if __name__ != "__main__":
     sys.exit(1)
 
 kw = {
-    "name": "4ch",
+    "name": "luppy",
     "version": "0.3.0",
-    "description": "Python wrapper for the 4chan JSON API.",
+    "description": "Python wrapper for the lupchan/tinyboard 4chan-style JSON API.",
     "long_description": __doc__,
-    "url": "https://github.com/plausibility/4ch",
+    "url": "https://github.com/feblehober123/luppy",
     "author": "plausibility",
     "author_email": "chris@gibsonsec.org",
     "license": "MIT",
-    "packages": ['fourch'],
+    "packages": ['luppy'],
     "install_requires": ["requests"],
     "zip_safe": False,
-    "keywords": "wrapper 4chan chan json",
+    "keywords": "wrapper lupchan chan json",
     "classifiers": [
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
