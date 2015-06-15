6b7db527885bc08c610f8896cdbf5de6117f1d52
frozenpigs <frozenpigs@cock.li>
2015-06-15 23:36:02 +0100

    flippidy doda and some new lines

--- run.py
+++ run.py
@@ -6,9 +6,11 @@ seenurls = []
 while True:
     recent = scraper.Recent()
     urls = recent.getrecent()
+    urls.reverse()
     for url in urls:
         if url not in seenurls:
             seenurls.append(url)
+            print()
             print(url)
             print(recent.getboard(url) + " " + " ".join(recent.getbody(url)))
     sleep(5)
